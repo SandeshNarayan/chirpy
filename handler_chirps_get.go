c81fe5d6f0d0629e2a456ea85f244da0a0d472d8
SandeshNarayan <sandeshnarayan81@gmail.com>
2025-02-02 12:07:20 +0530

    L: Implemented sorting query and authorID query for retrieving chirps

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -1,14 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request ){
-		
+	
+
+	authorIDString := r.URL.Query().Get("author_id")
+	sortDirection := r.URL.Query().Get("sort")
+
+	if sortDirection!="" && (sortDirection!="asc" && sortDirection!="desc"){
+        respondWithError(w, http.StatusBadRequest, "Invalid sort order", nil)
+        return
+    }
+	
+
+
 	dbchirps, err:= cfg.dbQueries.GetAllChirps(r.Context())
 	if err!=nil{
 		respondWithError(w, http.StatusInternalServerError, err.Error(), err)
 		return
 	}
+	
+	
+
+
+	if authorIDString!=""{
+
+		authorID, err := uuid.Parse(authorIDString)
+		if err!=nil{
+			respondWithError(w, http.StatusBadRequest, "Invalid user ID", err)
+			return
+		}
+
+		dbchirps, err = cfg.dbQueries.GetChirpByUserID(r.Context(), authorID)
+		if err!=nil{
+            respondWithError(w, http.StatusInternalServerError, err.Error(), err)
+            return
+        }
+	}
+	if sortDirection == "desc" {
+		sort.Slice(dbchirps, func(i, j int) bool {
+			return dbchirps[i].CreatedAt.After(dbchirps[j].CreatedAt)
+		})
+	}
+	
 	
 	chirps :=[]Chirp{}
 
